68bd7a92bdf074e3b53ded553fcdb34e83fb5574
soorajk1 <ksooraj162@gmail.com>
2023-08-24 20:44:12 +0530

    setup firebase, added login,signup page

--- netflix_clone/src/App.tsx
+++ netflix_clone/src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import { Home } from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
-      <Navbar /> //present in all routes so above all of them
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar /> //present in all routes so above all of them
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
---
+++ netflix_clone/src/context/AuthContext.tsx
@@ -0,0 +1,77 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  User as FirebaseUser,
+} from "firebase/auth";
+
+interface User {
+  uid: string;
+  email: string;
+}
+
+interface AuthContextType {
+  signUp: (email: string, password: string) => Promise<any>;
+  logIn: (email: string, password: string) => Promise<any>;
+  logOut: () => Promise<void>;
+  user: User | null;
+}
+
+interface AuthContextProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  async function signUp(email: string, password: string): Promise<any> {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  async function logIn(email: string, password: string): Promise<any> {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  async function logOut(): Promise<void> {
+    await signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (currUser: FirebaseUser | null) => {
+        if (currUser) {
+          const userObject: User = {
+            uid: currUser.uid,
+            email: currUser.email || "",
+          };
+          setUser(userObject);
+        } else {
+          setUser(null);
+        }
+      }
+    );
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function UserAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useUserAuth must be used within an AuthContextProvider");
+  }
+  return context;
+}
